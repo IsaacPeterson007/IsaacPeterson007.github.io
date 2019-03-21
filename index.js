bbbfd08e52811620700cf3aeb65e4c8710b23352
IsaacPeterson007 <brud.icp@gmail.com>
2019-03-21 11:52:11 -0500

    toggle visible

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-document.querySelector("button").addEventListener("click", (e)=> {
-    e.document.getElementById("button").style.backgroundColor = "#2980B9";
-  })
+AFRAME.registerComponent('map', {
+
+    init: function() {
+        const animatedMarker = document.querySelector("#marker");
+        const aEntity = document.querySelector("#model");
+
+        // every click, we make our model grow in size :)
+        animatedMarker.addEventListener('click', function(ev, target){
+            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+            if (aEntity && intersectedElement === aEntity) {
+                aEntity.setAttribute('visible', false);
+            }
+        });
+}});
