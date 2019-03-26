3dd7b0301e5e32babf84eb97a5de150cca2c331c
IsaacPeterson007 <brud.icp@gmail.com>
2019-03-26 11:11:33 -0500

    added script to index.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-var btnClicked = false;
-
-function clicked(btn){
-    if(!btnClicked){
-        var map = document.getElementById("model");
-        map.src = "Images/Map_NoIcons.png";
-        btn.textContext = "Map with no icons";
-        btnClicked = !btnClicked;
-    }
-    else{
-        var map = document.getElementById("model");
-        map.src = "Images/Map_Complete.png";
-        btn.textContext = "Map with icons";
-        btnClicked = !btnClicked;
-    }
-}
+document.querySelector('btn').addEventListener('click', function (evt) {
+    console.log('This 2D element was clicked!');
+  });
